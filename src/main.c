f9fc4999261ef1b0f2ace1ea8bea2dcc24c97c07
Wany917 <yvannkakou03@gmail.com>
2023-01-21 18:12:06 +0100

    test 1

--- src/main.c
+++ src/main.c
@@ -2,32 +2,11 @@
 
 int main()
 {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
     {
-        printf("Failed to initialize the SDL2 library\n");
-        return -1;
+        fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
-
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
-                                          0);
-
-    if(!window)
-    {
-        printf("Failed to create window\n");
-        return -1;
-    }
-
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-    if(!window_surface)
-    {
-        printf("Failed to get the surface from the window\n");
-        return -1;
-    }
-
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(5000);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
